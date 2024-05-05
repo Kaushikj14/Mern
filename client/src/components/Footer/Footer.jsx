5cc0bab8a324c2eec618bc6deb1038ca51204665
Kaushikj14 <kaushikjain1423@gmail.com>
2024-05-05 20:24:03 +0530

    registration with backend

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ export const Footer = ()=>{
     return <>
         <footer>
                 <p >Author : Kaushik Jain</p>
-                <p ><a href="mailto:kaushikjain0608@gmail.com">kaushikjain0608@gmail.com</a></p>
+                <p className="footer-text"><a href="mailto:kaushikjain0608@gmail.com">kaushikjain0608@gmail.com</a></p>
           
         </footer>
     </>
